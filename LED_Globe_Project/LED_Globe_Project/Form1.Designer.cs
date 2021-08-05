9f2020d7e1b2616ac1554dca3404454984121dae
jonaspetraska7 <jonas.petraska7@gmail.com>
2021-08-06 01:29:50 +0300

    UI dalis

---
+++ LED_Globe_Project/LED_Globe_Project/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace LED_Globe_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(41, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Įjungti Variklį";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 343);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(747, 95);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(41, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(194, 37);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Išjungti Variklį";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(486, 21);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(291, 40);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(261, 18);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(194, 43);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Atvaizduoti Tekstą";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // colorDialog1
+            // 
+            this.colorDialog1.AnyColor = true;
+            this.colorDialog1.FullOpen = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(261, 72);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(194, 42);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Atvaizduoti Spalvą";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(486, 72);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(48, 42);
+            this.button5.TabIndex = 6;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(261, 129);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(194, 42);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Atvaizduoti Paveikslėlį";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(41, 187);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(736, 139);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(486, 129);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(48, 42);
+            this.button7.TabIndex = 9;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ LED_Globe_Project/LED_Globe_Project/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LED_Globe_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button5.BackColor = colorDialog1.Color;
+            openFileDialog1.Title = "Ikelti Paveiksleli";
+        }
+        // Ijungti Varikli
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("[ON] Variklis Ijungtas !\r\n");
+        }
+
+        // Isjungti Varikli
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText("[OFF] Variklis Išjungtas !\r\n");
+        }
+
+        // Pasirinkti Spalva
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                button5.BackColor = colorDialog1.Color;
+        }
+
+        // Atvaizduoti Spalva
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText(string.Format("[COLOR] Atvaizduojama spalva R: {0}  G: {1}  B: {2} \r\n",
+                button5.BackColor.R, button5.BackColor.G, button5.BackColor.B));
+        }
+
+        // Atvaizduoti Teksta
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText(string.Format("[TEXT] Atvaizduojama tekstas : \"{0}\"  \r\n",
+                textBox2.Text));
+        }
+
+        // Atvaizduoti Paveiksleli
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.AppendText(string.Format("[IMAGE] Atvaizduojamas paveikslelis \"{0}\"  \r\n",
+                openFileDialog1.SafeFileName));
+        }
+
+        // Pasirinkti Paveiksleli
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var uploadedImage = Image.FromStream(openFileDialog1.OpenFile());
+                var resizedImage = resizeImage(360, 70, uploadedImage);
+                pictureBox1.Image = resizedImage;
+            }
+
+        }
+
+        public Image resizeImage(int newWidth, int newHeight, Image img)
+        {
+            Image imgPhoto = img;
+
+            int sourceWidth = imgPhoto.Width;
+            int sourceHeight = imgPhoto.Height;
+
+            //Consider vertical pics
+            if (sourceWidth < sourceHeight)
+            {
+                int buff = newWidth;
+
+                newWidth = newHeight;
+                newHeight = buff;
+            }
+
+            int sourceX = 0, sourceY = 0, destX = 0, destY = 0;
+            float nPercent = 0, nPercentW = 0, nPercentH = 0;
+
+            nPercentW = ((float)newWidth / (float)sourceWidth);
+            nPercentH = ((float)newHeight / (float)sourceHeight);
+            if (nPercentH < nPercentW)
+            {
+                nPercent = nPercentH;
+                destX = System.Convert.ToInt16((newWidth -
+                          (sourceWidth * nPercent)) / 2);
+            }
+            else
+            {
+                nPercent = nPercentW;
+                destY = System.Convert.ToInt16((newHeight -
+                          (sourceHeight * nPercent)) / 2);
+            }
+
+            int destWidth = (int)(sourceWidth * nPercent);
+            int destHeight = (int)(sourceHeight * nPercent);
+
+
+            Bitmap bmPhoto = new Bitmap(newWidth, newHeight,
+                          PixelFormat.Format24bppRgb);
+
+            bmPhoto.SetResolution(newWidth,
+                         newHeight);
+
+            Graphics grPhoto = Graphics.FromImage(bmPhoto);
+            grPhoto.Clear(Color.Black);
+            grPhoto.InterpolationMode =
+                System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+
+            grPhoto.DrawImage(imgPhoto,
+                new Rectangle(destX, destY, destWidth, destHeight),
+                new Rectangle(sourceX, sourceY, sourceWidth, sourceHeight),
+                GraphicsUnit.Pixel);
+
+            grPhoto.Dispose();
+            imgPhoto.Dispose();
+            bmPhoto.Save("LOWER_RES.jpg");
+            return bmPhoto;
+        }
+    }
+}
